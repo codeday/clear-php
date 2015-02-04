298ca99234a4f79e67379423ba1d53c8e19d0b9d
Tyler Menezes <tylermenezes@gmail.com>
2015-02-04 15:30:33 -0800

    Add Use Venue Address button for ship to

--- app/Controllers/Manage/Event/ShippingController.php
+++ app/Controllers/Manage/Event/ShippingController.php
@@ -19,15 +19,27 @@ class ShippingController extends \Controller {
             \App::abort(401);
         }
 
-        $event->ship_name = \Input::get('ship_name');
-        $event->ship_company = \Input::get('ship_company');
-        $event->ship_address_1 = \Input::get('ship_address_1');
-        $event->ship_address_2 = \Input::get('ship_address_2');
-        $event->ship_city = \Input::get('ship_city');
-        $event->ship_state = \Input::get('ship_state');
-        $event->ship_postal = \Input::get('ship_postal');
-        $event->ship_country = \Input::get('ship_country');
-        $event->ship_is_residential = \Input::get('ship_is_residential') ? true : false;
+        if (\Input::get('copy')) {
+            $event->ship_name = $event->venue_name;
+            $event->ship_company = "ATTN CODEDAY";
+            $event->ship_address_1 = $event->venue_address_1;
+            $event->ship_address_2 = $event->venue_address_2;
+            $event->ship_city = $event->venue_city;
+            $event->ship_state = $event->venue_state;
+            $event->ship_postal = $event->venue_postal;
+            $event->ship_country = $event->venue_country;
+            $event->ship_is_residential = false;
+        } else {
+            $event->ship_name = \Input::get('ship_name');
+            $event->ship_company = \Input::get('ship_company');
+            $event->ship_address_1 = \Input::get('ship_address_1');
+            $event->ship_address_2 = \Input::get('ship_address_2');
+            $event->ship_city = \Input::get('ship_city');
+            $event->ship_state = \Input::get('ship_state');
+            $event->ship_postal = \Input::get('ship_postal');
+            $event->ship_country = \Input::get('ship_country');
+            $event->ship_is_residential = \Input::get('ship_is_residential') ? true : false;
+        }
 
         \Session::flash('status_message', 'Shipping information updated');
 
