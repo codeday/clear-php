7501d798ef0f03910cce70f7a93e4343592d79fe
TJ Horner <rowenrocks@gmail.com>
2015-02-24 21:55:15 -0800

    Completely remove app/storage

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Setting up environment for Clear..."
+cd app
+mkdir storage
+cd storage
+mkdir cache logs meta sessions views
+cd ..
+cd config
+touch local.json
+cd ../..
+
+echo "Installing Composer dependencies..."
+composer install
