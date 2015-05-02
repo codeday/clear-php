349b8d8b9f07a85e1a7d5df8b4feeb8832544d3a
Tyler Menezes <tylermenezes@gmail.com>
2015-05-01 18:37:03 -0700

    Change default debug entry

--- app/routes/903-debug.php
+++ app/routes/903-debug.php
@@ -2,7 +2,7 @@
 
 \Route::when('debug/*', 'csrf', ['post']);
 \Route::group(['namespace' => 'Manage\Debug', 'prefix' => 'debug', 'before' => 's5_admin'], function() {
-    \Route::get('', function(){ return \Redirect::to('/debug/log'); });
+    \Route::get('', function(){ return \Redirect::to('/debug/decrypt'); });
     \Route::controller('/log', 'LogController');
     \Route::controller('/queue', 'QueueController');
     \Route::controller('/decrypt', 'DecryptController');
