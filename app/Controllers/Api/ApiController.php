e2367a97348494ac9db63cabc7562781fa4f5996
TJ Horner <me@tjhorner.com>
2015-09-16 19:02:03 -0700

    That operator was there...

--- app/Controllers/Api/ApiController.php
+++ app/Controllers/Api/ApiController.php
@@ -63,7 +63,7 @@ abstract class ApiController extends \Controller {
 
     public function getStats(){
       $this->requirePermission(["internal"]);
-      $registrations = \DB::table('batches_events_registrations')->->count();
+      $registrations = \DB::table('batches_events_registrations')->count();
 
       return json_encode([
         "registrations_all_time" => $registrations
