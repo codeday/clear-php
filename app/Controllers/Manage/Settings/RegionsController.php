b58c52b81ee42bf37900bd4ac16b5b805df026ad
TJ Horner <me@tjhorner.com>
2015-09-19 16:40:54 -0700

    Add edit region page [temporary]

--- app/Controllers/Manage/Settings/RegionsController.php
+++ app/Controllers/Manage/Settings/RegionsController.php
@@ -7,4 +7,21 @@ class RegionsController extends \Controller {
     {
         return \View::make('settings/regions/index');
     }
-} 
+
+    public function getEdit()
+    {
+        $region = \Route::input('region');
+        return \View::make('settings/regions/edit', ['region' => $region]);
+    }
+
+    public function postEdit()
+    {
+        $region = \Route::input('region');
+        $region->name = \Input::get('name');
+        $region->save();
+
+        \Session::flash('status_message', 'Region updated');
+
+        return \Redirect::to('/settings/regions/'.$region->id);
+    }
+}
