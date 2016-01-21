246a7d282ee4873918a5068e5bd1add1631bf959
Tyler Menezes <tylermenezes@gmail.com>
2016-01-21 15:27:53 -0800

    Add flyers
    Signed-off-by: Tyler Menezes <tylermenezes@gmail.com>

---
+++ app/Http/Controllers/Email/FlyerController.php
@@ -0,0 +1,85 @@
+<?php
+namespace CodeDay\Clear\Http\Controllers\Email;
+
+use CodeDay\Clear\Models;
+
+class FlyerController extends \CodeDay\Clear\Http\Controller
+{
+    public function getPoster()
+    {
+        $event = \Route::input('event');
+
+        $promotion = null;
+        if ($event) {
+            $promotion = Models\Batch\Event\Promotion
+                ::where('batches_event_id', '=', $event->id)
+                ->where('code', '=', \Input::get('code'))
+                ->first();
+        }
+
+        return $this->makeFlyer('poster', Models\Batch::Loaded(), $event, $promotion);
+    }
+
+    public function getHandout()
+    {
+        $event = \Route::input('event');
+        if (!isset($event)) {
+            \App::abort(404);
+        }
+        $promotion = Models\Batch\Event\Promotion
+            ::where('batches_event_id', '=', $event->id)
+            ->where('code', '=', \Input::get('code'))
+            ->first();
+
+        return $this->makeFlyer('handout', Models\Batch::Loaded(), $event, $promotion);
+    }
+
+    private function makeFlyer($view, Models\Batch $batch, Models\Batch\Event $event = null, Models\Batch\Event\Promotion $code = null)
+    {
+        $pdf = new \TCPDF('P', 'in', 'LETTER', true, 'UTF-8', false);
+
+        //set margins
+        $pdf->SetMargins(0.5, 0.5, 0.5);
+        $pdf->SetHeaderMargin(0);
+        $pdf->SetFooterMargin(0);
+
+        //set auto page breaks
+        $pdf->SetAutoPageBreak(false);
+
+        //set image scale factor
+        $pdf->setImageScale(1);
+
+        // set document information
+        $pdf->SetCreator('Clear');
+        $pdf->SetAuthor('StudentRND');
+        $pdf->SetTitle('CodeDay');
+        $pdf->SetSubject('CodeDay');
+        $pdf->SetKeywords('codeday');
+
+        // set default font subsetting mode
+        $pdf->setFontSubsetting(true);
+
+        $pdf->SetFont('dejavusans', '', 14, '', true);
+
+        $pdf->AddPage();
+
+        $html = \View::make('flyers/'.$view, ['batch' => $batch, 'event' => $event, 'code' => $code])->render();
+
+        $pdf->writeHTMLCell($w=0, $h=0, $x='', $y='', $html);
+
+        $bin = $pdf->Output($view.'.pdf', 'S');
+
+        $filename = 'codeday';
+        if ($event) {
+            $filename .= '-'.$event->webname;
+        }
+        if ($code) {
+            $filename .= '-'.strtolower($code->code);
+        }
+        $filename .= '-poster';
+
+        return response($bin)
+            ->header('Content-type', 'application/pdf')
+            ->header('Content-Disposition', 'inline; filename="'.$filename.'.pdf"');
+    }
+}
--- app/Http/routes/100-email.php
+++ app/Http/routes/100-email.php
@@ -2,5 +2,7 @@
 
 \Route::group(['namespace' => 'Email', 'prefix' => 'e', 'middleware' => ['web']], function() {
     \Route::controller('/incoming', 'IncomingController');
+    \Route::controller('/flyer/{event}', 'FlyerController');
+    \Route::controller('/flyer', 'FlyerController');
     \Route::controller('', 'IndexController');
 });
