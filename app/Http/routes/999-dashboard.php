478743b37944a452edd5571edabc1464e9cc747e
Tyler Menezes <tylermenezes@gmail.com>
2016-01-28 08:44:38 -0800

    Fix cross-site framing for Front
    Signed-off-by: Tyler Menezes <tylermenezes@gmail.com>

--- app/Http/routes/999-dashboard.php
+++ app/Http/routes/999-dashboard.php
@@ -1,11 +1,14 @@
 <?php
 \Route::group(['namespace' => 'Manage', 'before' => 's5_manage_events', 'middleware' => ['web']], function() {
     \Route::get('/', 'DashboardController@getIndex');
-    \Route::get('/front-plugin', 'DashboardController@getFrontPlugin');
-    \Route::get('/front-plugin-data', 'DashboardController@getFrontPluginData');
     \Route::get('/updates.json', 'DashboardController@getUpdates');
     \Route::get('/logout', function(){
         \CodeDay\Clear\Models\User::me()->forget();
         return \Redirect::to('https://s5.studentrnd.org/login/logout');
     });
+});
+
+\Route::group(['namespace' => 'Manage', 'before' => 's5_manage_events'], function() {
+    \Route::get('/front-plugin', 'DashboardController@getFrontPlugin');
+    \Route::get('/front-plugin-data', 'DashboardController@getFrontPluginData');
 });
