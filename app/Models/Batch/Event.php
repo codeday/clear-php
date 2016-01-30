9dca2dbccac8b0399a1145272ba06e14ef65583a
TJ Horner <me@tjhorner.com>
2016-01-30 13:14:48 -0800

    Floor early bird max registrations

--- app/Models/Batch/Event.php
+++ app/Models/Batch/Event.php
@@ -259,7 +259,7 @@ class Event extends \Eloquent {
 
     public function getEarlyBirdMaxRegistrationsAttribute()
     {
-        return $this->max_registrations * 0.3;
+        return floor($this->max_registrations * 0.3);
     }
 
     public function getUnregisteredNotifyAttribute()
