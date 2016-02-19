bd3cce5d544ddae37ad17f33557b4061c2bd584a
Tyler Menezes <tylermenezes@gmail.com>
2016-02-18 18:12:18 -0800

    Fix test failures (no extend CSRV verifier)

--- app/Http/Middleware/ShareBatch.php
+++ app/Http/Middleware/ShareBatch.php
@@ -3,23 +3,15 @@
 namespace CodeDay\Clear\Http\Middleware;
 
 use CodeDay\Clear\Models;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
-class ShareBatch extends BaseVerifier
+class ShareBatch
 {
-    /**
-     * The URIs that should be excluded from CSRF verification.
-     *
-     * @var array
-     */
-    protected $except = [
-        //
-    ];
-
     public function handle($request, \Closure $next)
     {
-        \View::share('loaded_batch', Models\Batch::Loaded());
-        \View::share('managed_batch', Models\Batch::Managed());
-        return parent::handle($request, $next);
+        if (Models\User::is_logged_in()) {
+            \View::share('loaded_batch', Models\Batch::Loaded());
+            \View::share('managed_batch', Models\Batch::Managed());
+        }
+        return $next($request);
     }
 }
--- app/Http/Middleware/ShareSessionErrors.php
+++ app/Http/Middleware/ShareSessionErrors.php
@@ -2,23 +2,13 @@
 
 namespace CodeDay\Clear\Http\Middleware;
 
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
-
-class ShareSessionErrors extends BaseVerifier
+class ShareSessionErrors
 {
-    /**
-     * The URIs that should be excluded from CSRF verification.
-     *
-     * @var array
-     */
-    protected $except = [
-        //
-    ];
-
     public function handle($request, \Closure $next)
     {
+        
         \View::share('error', \Session::get('error'));
         \View::share('status_message', \Session::get('status_message'));
-        return parent::handle($request, $next);
+        return $next($request);
     }
 }
