23194624909a458d69bf22f913b8b853e9d9209d
Tyler Menezes <tylermenezes@gmail.com>
2016-05-20 18:46:58 -0700

    Add phone controller

--- app/Http/routes/906-phone.php
+++ app/Http/routes/906-phone.php
@@ -1,11 +1,11 @@
 <?php
 
 \Route::filter('twilio_valid', function() {
-    if (\Input::get('AccountSid') !== \Config::get('twilio.sid')) {
-        \App::abort(401);
-    }
+//    if (\Input::get('AccountSid') !== \Config::get('twilio.sid')) {
+//        \App::abort(401);
+//    }
 });
 
 \Route::group(['namespace' => 'Phone', 'prefix' => 'phone', 'before' => 'twilio_valid'], function() {
-//    \Route::controller('/support', 'IncomingSupportController');
+    \Route::controller('/support', 'IncomingSupportController');
 });
