a5f948bec814faf3f0e75edfd9102c73127afb53
TJ Horner <me@tjhorner.com>
2016-06-17 23:51:50 -0700

    Add region in the new API route

--- app/ModelContracts/Grant.php
+++ app/ModelContracts/Grant.php
@@ -28,6 +28,14 @@ class Grant extends ModelContract
                 'type'          => 'Event',
                 'rich'          => true,
                 'value'         => function($model, $permissions) { return new Event($model->event, $permissions, true); }
+            ],
+
+            'region' => [
+                'name'          => 'Event',
+                'description'   => 'The region of the event that this grant gave the user access to.',
+                'type'          => 'Event',
+                'rich'          => true,
+                'value'         => function($model, $permissions) { return new Region($model->event->region, $permissions, true); }
             ]
         ];
     }
