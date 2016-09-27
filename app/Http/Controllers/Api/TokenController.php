adbf76564e15a40e65421e6e4509a1e943faa4d1
TJ Horner <tjhorner@srnd.org>
2016-09-28 00:53:46 +0100

    Create token controller (hopefully)

---
+++ app/Http/Controllers/Api/TokenController.php
@@ -0,0 +1,18 @@
+<?php
+namespace CodeDay\Clear\Http\Controllers\Api;
+
+use \CodeDay\Clear\Models;
+use \CodeDay\Clear\ModelContracts;
+
+class TokenController extends ApiController {
+    public function getToken()
+    {
+    	$user = Models\User::fromToken(\Route::input('token'));
+        
+        if(isset($user)){
+        	return json_encode(ModelContracts\Registration::Model($user, ["internal"]));
+       	}else{
+       		\App::abort(403);
+       	}
+    }
+}
