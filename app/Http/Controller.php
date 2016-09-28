38bfc32affed528b803032339961b260fe27e288
TJ Horner <me@tjhorner.com>
2016-09-27 19:41:46 -0700

    Hopefully, this doesn't break anything

--- app/Http/Controller.php
+++ app/Http/Controller.php
@@ -10,4 +10,16 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct(){
+      if(isset(Models\User::me())){
+        $revert = array('%21'=>'!', '%2A'=>'*', '%27'=>"'", '%28'=>'(', '%29'=>')');
+
+        $token_json = strtr(rawurlencode(json_encode((object)[
+          'eventId' => getDayOfEvent()->id,
+          'eventName' => getDayOfEvent()->fullName,
+          'token' => Models\User::me()->token
+        ])), $revert);
+      }
+    }
 }
