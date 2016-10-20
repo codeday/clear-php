3037e327408b68c8f8aac300e59385a0209425d3
TJ Horner <me@tjhorner.com>
2016-10-19 20:58:00 -0700

    Fix typo: lowercase k

--- app/Http/Middleware/ShareRaygunApiKey.php
+++ app/Http/Middleware/ShareRaygunApiKey.php
@@ -6,7 +6,7 @@ class ShareRaygunApiKey
 {
     public function handle($request, \Closure $next)
     {
-        \View::share('raygun_api_Key', \Config::get('raygun.api_key'));
+        \View::share('raygun_api_key', \Config::get('raygun.api_key'));
         return $next($request);
     }
 }
