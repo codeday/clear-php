ba3c505fa9753e221f0b370b725c0b7b0aeebdc0
TJ Horner <me@tjhorner.com>
2016-10-19 19:27:18 -0700

    We want errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -35,8 +35,8 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $e)
     {
-        parent::report($e);
         $raygun->SendException($e);
+        parent::report($e);
     }
 
     /**
--- app/Http/routes/903-debug.php
+++ app/Http/routes/903-debug.php
@@ -2,6 +2,7 @@
 
 \Route::group(['namespace' => 'Manage\Debug', 'prefix' => 'debug', 'before' => 's5_admin', 'middleware' => ['web']], function() {
     \Route::get('', function(){ return \Redirect::to('/debug/decrypt'); });
+    \Route::get('/error', function(){ throw new Exception('This is an intentional error.'); });
     \Route::controller('/log', 'LogController');
     \Route::controller('/queue', 'QueueController');
     \Route::controller('/decrypt', 'DecryptController');
