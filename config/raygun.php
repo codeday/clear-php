dd8f7c19f1f49575b4c1f211e5de1505ac37af8c
Tyler Menezes <tylermenezes@gmail.com>
2017-02-06 16:39:15 -0800

    Add new judge and mentor emails

--- config/raygun.php
+++ config/raygun.php
@@ -3,5 +3,5 @@
 $config = json_decode(file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'local.json'), true);
 
 return [
-    'api_key' => $config['raygun']['api_key']
+    'api_key' => $config['raygun']['api_key'] ?? ''
 ];
