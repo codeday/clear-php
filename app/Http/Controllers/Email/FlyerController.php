5de5bef82c871adce38be098cb72b22c9fcccd6f
Tyler Menezes <tylermenezes@gmail.com>
2017-04-28 18:25:57 -0700

    Update poster

--- app/Http/Controllers/Email/FlyerController.php
+++ app/Http/Controllers/Email/FlyerController.php
@@ -2,6 +2,7 @@
 namespace CodeDay\Clear\Http\Controllers\Email;
 
 use CodeDay\Clear\Models;
+define('FPDF_FONTPATH', base_path().'/resources/fonts');
 
 class FlyerController extends \CodeDay\Clear\Http\Controller
 {
@@ -17,7 +18,62 @@ class FlyerController extends \CodeDay\Clear\Http\Controller
                 ->first();
         }
 
-        return $this->makeFlyer('poster', Models\Batch::Loaded(), $event, $promotion);
+        $pdf = new \FPDI('P', 'mm', 'Letter');
+        $pdf->AddPage();
+        $pdf->setSourceFile(base_path().'/resources/pdf/poster.pdf');
+        $tplIdx = $pdf->importPage(1);
+        $pdf->useTemplate($tplIdx, 0, -1, 216);
+
+        // now write some text above the imported page
+        $pdf->AddFont('Proxima Nova', 'B', 'ProximaNova-Bold.php');
+        $pdf->SetFont('Proxima Nova', 'B');
+        $pdf->SetTextColor(255, 255, 255);
+        $pdf->SetFontSize(25);
+
+        // PDF details
+        $rightBlockMid = 157;
+        $pageMid = 108;
+
+        $centerText = function($xMidline, $y, $text) use ($pdf) {
+            $xStart = $xMidline - $pdf->GetStringWidth($text)/2;
+            $pdf->Text($xStart, $y, $text);
+        };
+
+        $eventStarts = $event ? $event->starts_at : Models\Batch::Loaded()->starts_at->timestamp;
+        $eventEnds = $event ? $event->ends_at : Models\Batch::Loaded()->ends_at->timestamp;
+        $textDate = date('F j-', $eventStarts).date('j', $eventEnds);
+        $centerText($rightBlockMid, 138, $textDate);
+
+        if ($event && $event->venue_name) {
+            $textHosted = 'HOSTED AT '.strtoupper($event->venue_name);
+            $pdf->SetFontSize(10);
+            $centerText($rightBlockMid, 160, $textHosted);
+        }
+
+        $textUrl = 'Get your tickets: codeday.org'.($event ? '/'.$event->webname : '');
+        $pdf->SetTextColor(0, 0, 0);
+        $pdf->SetFontSize(25);
+        $centerText(108, 256, $textUrl);
+
+        $textPromo = $promotion ? 'Promo code '.$promotion->code : 'Tickets sell out quickly!';
+        $pdf->SetFontSize(16);
+        $centerText(108, 265, $textPromo);
+
+        $filename = 'codeday';
+        if ($event) {
+            $filename .= '-'.$event->webname;
+        }
+        if ($promotion) {
+            $filename .= '-'.strtolower($promotion->code);
+        }
+        $filename .= '-poster.pdf';
+
+
+        $bin = $pdf->Output($filename, 'S');
+
+        return response($bin)
+            ->header('Content-type', 'application/pdf')
+            ->header('Content-Disposition', 'inline; filename="'.$filename.'"');
     }
 
     public function getHandout()
