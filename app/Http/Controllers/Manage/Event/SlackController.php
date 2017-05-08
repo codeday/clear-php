a541546b829adb60362afe7f9f0c84e0cd0512d2
TJ Horner <me@tjhorner.com>
2017-05-07 18:07:22 -0700

    make the cool slack controller in the api folder :)

--- app/Http/Controllers/Manage/Event/SlackController.php
+++ app/Http/Controllers/Manage/Event/SlackController.php
@@ -1,37 +1,9 @@
 <?php
 namespace CodeDay\Clear\Http\Controllers\Manage\Event;
 
-use \CodeDay\Clear\Models;
-use \CodeDay\Clear\Services;
-
 class SlackController extends \CodeDay\Clear\Http\Controller {
   public function getIndex()
   {
     return \View::make('event/slack');
-  }
-
-  public function getOauth()
-  {
-    $code = \Input::get('code');
-    $event = Models\User::me()->current_managed_events->sortBy('date_created')->last();
-    $oauth_data = Services\Slack::GetOauthAccess($code);
-
-    if($oauth_data && $event){
-      $webhook = new Models\Application\Webhook;
-      $webhook->url = $oauth_data['incoming_webhook']['url'];
-      $webhook->event = "slack.registration.register.".$event->id;
-      $webhook->application_id = \Config::get('slack.internal_app');
-      $webhook->save();
-
-      Services\Slack::SendPayloadToUrl([
-        'text' => ":wave: Hello! If you see this, that means your Clear integration is working. Try registering someone through Clear!"
-      ], $oauth_data['incoming_webhook']['url']);
-
-      \Session::flash('status_message', 'Connected to Slack! We sent a test message, go look for it.');
-    }else{
-      \Session::flash('error', 'Could not authorize you with Slack. Please try again.');
-    }
-
-    \Redirect::to('/event/' . $event->id . '/slack');
   }
 }
