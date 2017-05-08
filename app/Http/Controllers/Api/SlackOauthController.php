df930f18b8771ab790bc780d701d60080853145d
TJ Horner <me@tjhorner.com>
2017-05-07 18:20:08 -0700

    That's not how you access json decoded objects

--- app/Http/Controllers/Api/SlackOauthController.php
+++ app/Http/Controllers/Api/SlackOauthController.php
@@ -13,14 +13,14 @@ class SlackOauthController extends \CodeDay\Clear\Http\Controller {
 
     if($oauth_data && $event){
       $webhook = new Models\Application\Webhook;
-      $webhook->url = $oauth_data['incoming_webhook']['url'];
+      $webhook->url = $oauth_data->incoming_webhook->url;
       $webhook->event = "slack.registration.register.".$event->id;
       $webhook->application_id = \Config::get('slack.internal_app');
       $webhook->save();
 
       Services\Slack::SendPayloadToUrl([
         'text' => ":wave: Hello! If you see this, that means your Clear integration is working. Try registering someone through Clear!"
-      ], $oauth_data['incoming_webhook']['url']);
+      ], $oauth_data->incoming_webhook->url);
 
       \Session::flash('status_message', 'Connected to Slack! We sent a test message, go look for it.');
     }else{
