7fd5bf70e95a7c89320a0325ffddbabe69d65740
TJ Horner <me@tjhorner.com>
2017-05-07 18:47:18 -0700

    I FORGOT TO PUT IN THE CONFIG :((

--- config/slack.php
+++ config/slack.php
@@ -3,5 +3,8 @@
 $config = json_decode(file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'local.json'), true);
 
 return [
-    'webhook_url' => $config['slack']['webhook_url']
+    'webhook_url' => $config['slack']['webhook_url'],
+    'client_id' => $config['slack']['client_id'],
+    'client_secret' => $config['slack']['client_secret'],
+    'internal_app' => $config['slack']['internal_app']
 ];
