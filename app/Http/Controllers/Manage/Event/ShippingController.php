9b8fb081a98ad89bc4ff2320bba0c68ddf94910d
Tyler Menezes <tylermenezes@gmail.com>
2017-05-11 12:30:36 -0700

    Add last season shipping copy

--- app/Http/Controllers/Manage/Event/ShippingController.php
+++ app/Http/Controllers/Manage/Event/ShippingController.php
@@ -29,6 +29,16 @@ class ShippingController extends \CodeDay\Clear\Http\Controller {
             $event->ship_postal = $event->venue_postal;
             $event->ship_country = $event->venue_country;
             $event->ship_is_residential = false;
+        } elseif (\Input::get('past')) {
+            $event->ship_name = $event->previous_event->ship_name;
+            $event->ship_company = $event->previous_event->ship_company;
+            $event->ship_address_1 = $event->previous_event->ship_address_1;
+            $event->ship_address_2 = $event->previous_event->ship_address_2;
+            $event->ship_city = $event->previous_event->ship_city;
+            $event->ship_state = $event->previous_event->ship_state;
+            $event->ship_postal = $event->previous_event->ship_postal;
+            $event->ship_country = $event->previous_event->ship_country;
+            $event->ship_is_residential = $event->previous_event->ship_is_residential;
         } else {
             $event->ship_name = \Input::get('ship_name');
             $event->ship_company = \Input::get('ship_company');
