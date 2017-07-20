437473010fa1d77bb1827c5f7736f2d78a2d9564
TJ Horner <me@tjhorner.com>
2017-07-19 21:06:44 -0700

    Revert to queueing things one-by-one

--- app/Services/Notifications.php
+++ app/Services/Notifications.php
@@ -6,29 +6,27 @@ use CodeDay\Clear\Models\Batch\Event\Registration;
 
 class Notifications {
   public static function SendNotificationsForAnnouncement($announcement) {
-    \Queue::push(function($job) use ($announcement) {
-      $event = $announcement->event;
-      $registrations = $event->registrations;
+    $event = $announcement->event;
+    $registrations = $event->registrations;
 
-      foreach($registrations as $registration) {
-        $devices = $registration->devices;
+    foreach($registrations as $registration) {
+      $devices = $registration->devices;
 
-        foreach($devices as $device) {
-          switch($device->service) {
-            case "messenger":
-              FacebookMessenger::SendMessage("An announcement from the CodeDay organizers:\n\n" . $announcement->body, $device->token);
-              break;
-            case "sms":
-              Telephony\Sms::send($device->token, "CodeDay Announcement: " . $announcement->body);
-              break;
-            case "app":
-              // TODO companion implementation
-              break;
-          }
+      foreach($devices as $device) {
+        switch($device->service) {
+          case "messenger":
+            FacebookMessenger::SendMessage("An announcement from the CodeDay organizers:\n\n" . $announcement->body, $device->token);
+            break;
+          case "sms":
+            Telephony\Sms::send($device->token, "CodeDay Announcement: " . $announcement->body);
+            break;
+          case "app":
+            // TODO companion implementation
+            break;
         }
       }
+    }
 
-      $job->delete();
-    });
+    $job->delete();
   }
 }
