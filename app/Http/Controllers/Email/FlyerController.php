dfb9ce2ac797a04270bfa9a2622d2573d86a842e
Tyler Menezes <tylermenezes@gmail.com>
2017-09-18 11:59:13 -0700

    Update handout

--- app/Http/Controllers/Email/FlyerController.php
+++ app/Http/Controllers/Email/FlyerController.php
@@ -106,65 +106,65 @@ class FlyerController extends \CodeDay\Clear\Http\Controller
     public function getHandout()
     {
         $event = \Route::input('event');
-        if (!isset($event)) {
-            \App::abort(404);
+
+        $promotion = null;
+        if ($event) {
+            $promotion = Models\Batch\Event\Promotion
+                ::where('batches_event_id', '=', $event->id)
+                ->where('code', '=', \Input::get('code'))
+                ->first();
         }
-        $promotion = Models\Batch\Event\Promotion
-            ::where('batches_event_id', '=', $event->id)
-            ->where('code', '=', \Input::get('code'))
-            ->first();
 
-        return $this->makeFlyer('handout', Models\Batch::Loaded(), $event, $promotion);
-    }
-
-    private function makeFlyer($view, Models\Batch $batch, Models\Batch\Event $event = null, Models\Batch\Event\Promotion $code = null)
-    {
-        $pdf = new \TCPDF('P', 'in', 'LETTER', true, 'UTF-8', false);
-
-        //set margins
-        $pdf->SetMargins(0.5, 0.5, 0.5);
-        $pdf->SetHeaderMargin(0);
-        $pdf->SetFooterMargin(0);
-        $pdf->SetPrintHeader(false);
-        $pdf->SetPrintFooter(false);
-
-        //set auto page breaks
-        $pdf->SetAutoPageBreak(false);
-
-        //set image scale factor
-        $pdf->setImageScale(1);
-
-        // set document information
-        $pdf->SetCreator('Clear');
-        $pdf->SetAuthor('StudentRND');
-        $pdf->SetTitle('CodeDay');
-        $pdf->SetSubject('CodeDay');
-        $pdf->SetKeywords('codeday');
-
-        // set default font subsetting mode
-        $pdf->setFontSubsetting(true);
-
-        $pdf->SetFont('dejavusans', '', 14, '', true);
-
+        $pdf = new \FPDI('P', 'mm', 'Letter');
         $pdf->AddPage();
+        $pdf->setSourceFile(base_path().'/resources/pdf/handout.pdf');
+        $tplIdx = $pdf->importPage(1);
+        $pdf->useTemplate($tplIdx, 0, -1, 216);
 
-        $html = \View::make('flyers/'.$view, ['batch' => $batch, 'event' => $event, 'code' => $code])->render();
+        // now write some text above the imported page
+        $pdf->AddFont('Proxima Nova', 'B', 'ProximaNova-Bold.php');
+        $pdf->AddFont('Proxima Nova', '', 'ProximaNova-Regular.php');
+        $pdf->SetFont('Proxima Nova', '');
+        $pdf->SetTextColor(0, 0, 0);
+        $pdf->SetFontSize(12);
 
-        $pdf->writeHTMLCell($w=0, $h=0, $x='', $y='', $html);
+        // PDF details
+        $pageMid = 108;
 
-        $bin = $pdf->Output($view.'.pdf', 'S');
+        $centerText = function($xMidline, $y, $text) use ($pdf) {
+            $xStart = $xMidline - $pdf->GetStringWidth($text)/2;
+            $pdf->Text($xStart, $y, $text);
+            $pdf->Text($xStart, $y+140, $text);
+        };
+
+        $eventStarts = $event ? $event->starts_at : Models\Batch::Loaded()->starts_at->timestamp;
+        $eventEnds = $event ? $event->ends_at : Models\Batch::Loaded()->ends_at->timestamp;
+        $textDate = date('F j-', $eventStarts).date('j', $eventEnds);
+        $pdf->SetFont('Proxima Nova', 'B');
+        $centerText($pageMid, 29, $textDate);
+
+        $textUrl = 'For more info and to get your tickets, visit: codeday.org'.($event ? '/'.$event->webname : '');
+        $pdf->SetFont('Proxima Nova', 'B');
+        $centerText($pageMid, 59, $textUrl);
+
+        $textPromo = $promotion ? 'Use promo code '.$promotion->code.' for '.round($promotion->percent_discount,0).'% off' : 'Tickets sell out quickly!';
+        $pdf->SetFont('Proxima Nova', '');
+        $centerText($pageMid, 64, $textPromo);
 
         $filename = 'codeday';
         if ($event) {
             $filename .= '-'.$event->webname;
         }
-        if ($code) {
-            $filename .= '-'.strtolower($code->code);
+        if ($promotion) {
+            $filename .= '-'.strtolower($promotion->code);
         }
-        $filename .= '-'.$view;
+        $filename .= '-handout.pdf';
+
+
+        $bin = $pdf->Output($filename, 'S');
 
         return response($bin)
             ->header('Content-type', 'application/pdf')
-            ->header('Content-Disposition', 'inline; filename="'.$filename.'.pdf"');
+            ->header('Content-Disposition', 'inline; filename="'.$filename.'"');
     }
 }
