be299918b31e287c3f4a02c287d83764f571611a
Tyler Menezes <tylermenezes@gmail.com>
2018-02-07 14:40:46 -0800

    Remove requirement for all ship amounts

--- app/Http/Controllers/Manage/Batch/ShipmentController.php
+++ app/Http/Controllers/Manage/Batch/ShipmentController.php
@@ -32,23 +32,11 @@ class ShipmentController extends \CodeDay\Clear\Http\Controller {
 
     public function postPush()
     {
-        $eventsMissingShipInfo = Models\Batch\Event
-                               ::where('batch_id', '=', Models\Batch::Managed()->id)
-                               ->where(function($query) {
-                                   return $query->whereNull('ship_for')
-                                                 ->orWhereNull('ship_address_1')
-                                                 ->orWhereNull('ship_city')
-                                                 ->orWhereNull('ship_state')
-                                                 ->orWhereNull('ship_postal');
-                               })->count();
-
-        if ($eventsMissingShipInfo > 0) {
-            \Session::flash('error', $eventsMissingShipInfo.' events are missing shipment info');
-            return \Redirect::to('/batch/shipments/push');
-        }
-
         $count = 0;
         foreach (Models\Batch::Managed()->events as $event) {
+            if (!$event->ship_for || !$event->ship_address_1 || !$event->ship_city || !$event->ship_state || !$event->ship_postal)
+                continue;
+
             $order = Services\Ship::ToEvent('EVT-'.$event->id, $event, $event->manifestGenerated);
             Services\Ship::Tag($order, \Config::get('shipstation.tags.event_supplies'));
 
