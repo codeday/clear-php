3cfa1d04b9554bdf9cb48673fc2605aeb9add796
Tyler Menezes <tylermenezes@gmail.com>
2018-03-04 15:17:47 -0800

    Show type for registrations

--- app/Http/events/registration.php
+++ app/Http/events/registration.php
@@ -5,7 +5,7 @@ use \CodeDay\Clear\Services;
 \Event::listen('registration.register', function($reg)
 {
     Services\Slack::Message('<https://clear.codeday.org/event/'
-        .$reg->event->id.'/registrations/attendee/'.$reg->id.'|'.$reg->name.'>'
+        .$reg->event->id.'/registrations/attendee/'.$reg->id.'|'.$reg->name.'> ('.$reg->type.')'
         .' registered for CodeDay '.$reg->event->name);
 });
 
