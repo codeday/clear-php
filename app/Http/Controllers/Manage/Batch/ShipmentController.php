c7ab05701caf6b9887f93e3b7ffab1c82d3df9fd
Tyler Menezes <tylermenezes@gmail.com>
2019-02-04 08:08:14 -0800

    Add shipment suffix

--- app/Http/Controllers/Manage/Batch/ShipmentController.php
+++ app/Http/Controllers/Manage/Batch/ShipmentController.php
@@ -32,12 +32,13 @@ class ShipmentController extends \CodeDay\Clear\Http\Controller {
 
     public function postPush()
     {
+        $suffix = \Input::get('suffix') ? '-'.\Input::get('suffix') : '';
         $count = 0;
         foreach (Models\Batch::Managed()->events as $event) {
             if (!$event->ship_address_1 || !$event->ship_city || !$event->ship_state || !$event->ship_postal)
                 continue;
 
-            $order = Services\Ship::ToEvent('EVT-'.$event->id, $event, $event->manifestGenerated);
+            $order = Services\Ship::ToEvent('EVT-'.$event->id.$suffix, $event, $event->manifestGenerated);
             Services\Ship::Tag($order, \Config::get('shipstation.tags.event_supplies'));
 
 
